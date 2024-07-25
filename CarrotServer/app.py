5c88be7ca3efa747e76d7e0773104d6c3b690328
Minami127 <151480603+Minami127@users.noreply.github.com>
2024-07-25 15:11:36 +0900

    add

---
+++ CarrotServer/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+
+app = Flask(__name__)
+# 환경변수 셋팅
+
+#jwt 매니저 초기화
+jwt - JWTManager(app)
+
+api = Api(app)
+
+
+if __name__ == '__main__' :
+    app.run()
