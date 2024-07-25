70905b1e343ee56e3b64111a1e4cfb92351e756d
Minami127 <151480603+Minami127@users.noreply.github.com>
2024-07-25 17:33:12 +0900

    add

--- CarrotServer/app.py
+++ CarrotServer/app.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from config import Config
+
+from resources.user import UserRegisterResource
 
 app = Flask(__name__)
+
+api = Api(app)
+
 # 환경변수 셋팅
+
 app.config.from_object(Config)
 
 #jwt 매니저 초기화
-jwt - JWTManager(app)
+jwt = JWTManager(app)
 
-api = Api(app)
+
+api.add_resource(UserRegisterResource, '/user/register')
 
 
 if __name__ == '__main__' :
---
+++ CarrotServer/resources/user.py
@@ -0,0 +1,68 @@
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource
+from mysql_connection import get_connection
+from mysql.connector import Error
+
+from email_validator import EmailNotValidError, validate_email
+
+from utils import check_password, hash_password
+
+class UserRegisterResource(Resource) :
+
+    def post(self) :
+
+        data = request.get_json()
+
+        try : 
+            validate_email(data['email'])
+        except EmailNotValidError as e :
+            print(e)
+            return{'error' : str(e)}, 400
+        
+        if len(data['password']) < 4 or len(data['password']) > 12 :
+            return {'error' : '비밀번호 길이를 확인하세요'}, 400
+        
+        password = hash_password(data['password'])
+
+        print(password)
+
+
+        try :
+            connection = get_connection()
+
+            query = '''insert into users
+                        (email,password,nickname)
+                        values
+                        (%s,%s,%s)'''
+            
+            record = (data['email'],
+                      password,
+                      data['nickname'])
+            
+            cursor = connection.cursor()
+            cursor.execute(query,record)
+            connection.commit()
+
+            user_id = cursor.lastrowid
+
+            cursor.close()
+            connection.close()
+
+        except Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return{"error" : str(e)}, 500
+        
+        access_token = create_access_token(user_id)
+
+        return{'result' : 'success',
+               'accessToken' : access_token},200
+
+
+            
+
+
+
+
